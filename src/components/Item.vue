30a671ab88cd6da7272d807abee2a1f8b54b06b4
RusAlex91 <aocrusalex91@yandex.ru>
2021-10-06 11:01:43 +0300

    another fix

--- src/components/Item.vue
+++ src/components/Item.vue
@@ -1,12 +1,12 @@
 <template>
-  <div class="item" :class="{ opac: sold }">
-    <img src="" alt="" class="item__image" />
-    <h2 class="item__title">Lorem ipsum dolor</h2>
+  <div class="item" :class="{ paintSold: sold }">
+    <img :src="images[0]" alt="" class="item__image" />
+    <h2 class="item__title">"{{ name }}"</h2>
     <span v-if="sold" class="item__sold">Продана на аукционе</span>
     <div v-else class="item__wrapper">
       <div class="item-price-wrapper">
-        <span class="item__price discount">20000$</span>
-        <span class="item__price">10000$</span>
+        <span class="item__price discount">{{ newPrice }}$</span>
+        <span class="item__price">{{ price }}$</span>
       </div>
       <base-button>Купить</base-button>
     </div>
@@ -24,12 +24,16 @@ export default {
       type: Array,
       required: false
     },
+    desc: {
+      type: String,
+      required: false
+    },
     price: {
-      type: Boolean,
+      type: Number,
       required: false
     },
     newPrice: {
-      type: Boolean,
+      type: Number,
       required: false
     },
     sold: {
@@ -57,6 +61,14 @@ h2 {
 .item__wrapper {
   display: flex;
   column-gap: 24px;
+}
+
+.item__sold {
+  margin-top: 20px;
+}
+
+.paintSold {
+  opacity: 0.5;
 }
 
 .item__price {
